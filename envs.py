9d7bddc9c4074680712574804cc9dd53d121e0de
Zura Isakadze <z.isakadze@gmail.com>
2017-04-02 21:49:12 +0300

    small fixup

--- envs.py
+++ envs.py
@@ -1,12 +1,7 @@
 import gym
 import numpy as np
-import universe
 from gym.spaces.box import Box
-from universe import vectorized
-from universe.wrappers import Unvectorize, Vectorize
-
 import cv2
-
 
 # Taken from https://github.com/openai/universe-starter-agent
 def create_atari_env(env_id):
@@ -29,7 +24,7 @@ def _process_frame42(frame):
     frame = frame.mean(2)
     frame = frame.astype(np.float32)
     frame *= (1.0 / 255.0)
-    frame = np.reshape(frame, [1, 42, 42])
+    #frame = np.reshape(frame, [1, 42, 42])
     return frame
 
 class MyAtariRescale42x42(gym.ObservationWrapper):
@@ -50,59 +45,14 @@ class MyNormalizedEnv(gym.ObservationWrapper):
         self.alpha = 0.9999
         self.num_steps = 0
 
-    def _observation(self, observation_n):
-        for observation in observation_n:
-            self.num_steps += 1
-            self.state_mean = self.state_mean * self.alpha + \
-                observation.mean() * (1 - self.alpha)
-            self.state_std = self.state_std * self.alpha + \
-                observation.std() * (1 - self.alpha)
+    def _observation(self, observation):
+        self.num_steps += 1
+        self.state_mean = self.state_mean * self.alpha + \
+            observation.mean() * (1 - self.alpha)
+        self.state_std = self.state_std * self.alpha + \
+            observation.std() * (1 - self.alpha)
 
         unbiased_mean = self.state_mean / (1 - pow(self.alpha, self.num_steps))
         unbiased_std = self.state_std / (1 - pow(self.alpha, self.num_steps))
         ret = (observation - unbiased_mean) / (unbiased_std + 1e-8)
         return np.expand_dims(ret, axis=0)
-    
-
-
-
-
-
-
-
-
-
-
-
-class AtariRescale42x42(vectorized.ObservationWrapper):
-
-    def __init__(self, env=None):
-        super(AtariRescale42x42, self).__init__(env)
-        self.observation_space = Box(0.0, 1.0, [1, 42, 42])
-
-    def _observation(self, observation_n):
-        return [_process_frame42(observation) for observation in observation_n]
-
-
-class NormalizedEnv(vectorized.ObservationWrapper):
-
-    def __init__(self, env=None):
-        super(NormalizedEnv, self).__init__(env)
-        self.state_mean = 0
-        self.state_std = 0
-        self.alpha = 0.9999
-        self.num_steps = 0
-
-    def _observation(self, observation_n):
-        for observation in observation_n:
-            self.num_steps += 1
-            self.state_mean = self.state_mean * self.alpha + \
-                observation.mean() * (1 - self.alpha)
-            self.state_std = self.state_std * self.alpha + \
-                observation.std() * (1 - self.alpha)
-
-        unbiased_mean = self.state_mean / (1 - pow(self.alpha, self.num_steps))
-        unbiased_std = self.state_std / (1 - pow(self.alpha, self.num_steps))
-        import ipdb; ipdb.set_trace()
-        ret =  [(observation - unbiased_mean) / (unbiased_std + 1e-8) for observation in observation_n]
-        return ret 
